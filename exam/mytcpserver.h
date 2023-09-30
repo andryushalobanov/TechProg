8e0cb8c0d28c8e1786faefa6355d15cca177fcd4
Damil <113761992+damilpanteleev@users.noreply.github.com>
2023-09-30 14:03:54 +0300

    Add files via upload

---
+++ exam/mytcpserver.h
@@ -0,0 +1,46 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+
+
+#include <QObject>
+#include <QTcpServer> //tcp сервер (прослушивание канала, сигнал о подключении - все, что уже готово)
+#include <QTcpSocket>
+#include <QVector>
+
+//в cpp пишем все include
+#include <QtNetwork> //указание порта, хоста
+#include <QByteArray> //передаются биты
+#include <QDebug> //вывод в консоль
+#include <QRandomGenerator>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+
+    explicit MyTcpServer(QObject *parent = nullptr); //конструктор
+    //уничтожает экземпляра сервера и чистит памяти
+
+    ~MyTcpServer(); //деструктор
+public slots: //вызывается в случае срабатывания какого-либо сигнала
+
+
+    // функция установки соединения с сервером
+    void slotNewConnection();
+
+    // функция отключения от сурвера
+    void slotClientDisconnected();
+    //чтение данных из активного сокета
+    void slotServerRead();
+    void sendToClient(int pos, QString otvet);
+    //void slotReadClient();
+private:
+    int liveclients = 0;
+
+    QTcpServer * mTcpServer;
+    QVector<QTcpSocket *> mTcpSocket;
+    int server_status;
+
+};
+
+#endif // MYTCPSERVER_H
